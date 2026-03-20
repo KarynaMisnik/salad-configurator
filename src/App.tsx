54fb905c735d0297925c109032106ed2f367d68e
Muruseni <214897773+Muruseni@users.noreply.github.com>
2026-03-20 11:14:43 +0200

    footer component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-     
-    </>
+
+      <main className="flex-1">
+        {/* Main content goes here */}
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
