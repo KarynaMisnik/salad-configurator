ced0a3b1c7c9e605add121fc98d5294010d2235b
Muruseni <214897773+Muruseni@users.noreply.github.com>
2026-03-23 16:10:44 +0200

    removed imports from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
-import React from "react";
-
-import Header from "./components/Header";
-import Footer from "./components/footer";
-import BowlSelection from "./components/BowlSelection";
-
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-white font-sans">
-      <Header />
-
-      <BowlSelection />
-      
-      <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
-        
-
-        {/* Main content goes here */}
-      </main>
-
-      <Footer />
-    </div>
+   <></>
   );
 }
 
