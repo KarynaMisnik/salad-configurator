f4b8d2578a25859328cd2915311a89f499b0e014
Muruseni <214897773+Muruseni@users.noreply.github.com>
2026-03-23 16:04:15 +0200

    removed imports from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
-import Header from "./components/Header";
-import Footer from "./components/footer";
-import BowlSelection from "./components/BowlSelection";
-import IngredientSection from "./components/IngredientSection";
-import SummaryBar from "./components/SummaryBar";
-
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-white font-sans">
-      <Header />
-
-      <BowlSelection />
-      <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
-        <IngredientSection />
-        <SummaryBar />
-
-        {/* Main content goes here */}
-      </main>
-
-      <Footer />
-    </div>
+    <></>
   );
 }
 
