8f57782e0cb0215cc5f504de6d93af99f8bcf661
KarynaMisnik <karinamisnik94@gmail.com>
2026-03-23 11:17:23 +0200

    imports removed/ avoid potential conflicts while merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import React from "react";
-import CenterBowl from "./components/CenterBowl";
-
 function App() {
-  return (
-    <>
-      <CenterBowl />
-      <h1>Hello, I am empty and ready for development!</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
