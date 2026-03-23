b7cd5fe23f83560b0c9c4905a1ee9d1ed9eb5c99
KarynaMisnik <karinamisnik94@gmail.com>
2026-03-23 11:14:23 +0200

    App.css import removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-import "./App.css";
-
 function App() {
-  return (
-    <>
-      <h1>Hello, I am empty and ready for development!</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
