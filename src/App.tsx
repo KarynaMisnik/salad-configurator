47ec2ca3bd3ab00c102a47c725148b8b80bbd4dc
Muruseni <214897773+Muruseni@users.noreply.github.com>
2026-03-24 15:40:05 +0200

    fixed App.tsx Imported header  and footer and aded an outermost div

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import CenterBowl from "./components/CenterBowl";
 import BaseSelection from "./components/BaseSelection";
 import IngredientSection from "./components/IngredientSection";
 import SummaryBar from "./components/SummaryBar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+    <div className="min-h-screen flex flex-col bg-white font-sans">
+      <Header />
+
       <main>
         <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
           <BowlSelection />
@@ -16,6 +21,9 @@ function App() {
           <SummaryBar />
         </div>
       </main>
+
+      <Footer />
+      </div>
     </>
   );
 }
