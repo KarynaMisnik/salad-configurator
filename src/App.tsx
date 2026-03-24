eb40094c8fa0fa1a7d03d9a97ca2ad3d23c37a16
KarynaMisnik <karinamisnik94@gmail.com>
2026-03-24 15:23:59 +0200

    render several components, App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import BowlSelection from "./components/BowlSelection";
+import CenterBowl from "./components/CenterBowl";
+import BaseSelection from "./components/BaseSelection";
+import IngredientSection from "./components/IngredientSection";
+import SummaryBar from "./components/SummaryBar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
+          <BowlSelection />
+          <CenterBowl />
+          <BaseSelection />
+          <IngredientSection />
+          <SummaryBar />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
