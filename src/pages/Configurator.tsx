71a83a2edae73f5a1b3c60807a466687a4417c04
Muruseni <214897773+Muruseni@users.noreply.github.com>
2026-03-27 13:59:05 +0200

    Defined data states for Config file

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import BowlSelection from "../components/BowlSelection";
 import CenterBowl from "../components/CenterBowl";
 import BaseSelection from "../components/BaseSelection";
 import IngredientSection from "../components/IngredientSection";
 import SummaryBar from "../components/SummaryBar";
+import type { Bowl, Category, Ingredient } from "../types";
 
 function Configurator() {
+  const [bowls, setBowls] = useState<Bowl[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+
   return (
     <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
       <BowlSelection />
