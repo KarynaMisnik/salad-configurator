277103bb6a08aecad13139bc89768899664a2bf0
KarynaMisnik <karinamisnik94@gmail.com>
2026-04-10 13:54:45 +0300

    test depend added, vitest added

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,19 @@
+const BASE_URL = "http://localhost:3000/api";
+
 export async function getBowls(){
-    const response = await fetch("https://fresse-api.onrender.com/api/bowls");
+    const response = await fetch(BASE_URL+"/bowls");
     const data = await response.json();
     return data;
 }
 
 export async function getCategories(){
- const response = await fetch("https://fresse-api.onrender.com/api/categories");
+ const response = await fetch(BASE_URL+"/categories");
     const data = await response.json();
     return data;
 }
 
 export async function getIngredients(){
- const response = await fetch("https://fresse-api.onrender.com/api/ingredients");
+ const response = await fetch(BASE_URL+"/ingredients");
     const data = await response.json();
     return data;
 }
