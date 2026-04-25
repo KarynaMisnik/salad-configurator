ceb492d0f0de667fa2ef004b49e64fcc64bfc003
Muruseni <214897773+Muruseni@users.noreply.github.com>
2026-04-25 12:36:13 +0300

    More UI Polish selected base highlighted

--- src/components/BowlSelection.tsx
+++ src/components/BowlSelection.tsx
@@ -7,6 +7,7 @@ type Props = {
 
 export default function BowlSelection({ bowls }: Props) {
   const setBowl = useIngredientStore((state) => state.setBowl);
+  const selectedBowl = useIngredientStore((state) => state.selectedBowl);
   return (
     <aside className="bg-zinc-800 rounded-[3rem] p-6 text-white  lg:w-1/4 m-4 flex flex-col items-center shadow-lg">
       <div className="bg-white text-black font-bold rounded-full w-8 h-8 flex items-center justify-center mb-4 shrink-0">
@@ -15,20 +16,24 @@ export default function BowlSelection({ bowls }: Props) {
       <h2 className="text-xl font-bold mb-4">Valitse rasia</h2>
 
       <div className="w-full flex flex-col gap-3">
-        {bowls.map((bowl) => (
-          <div key={bowl.id} className="flex justify-center items-center">
-            <img
-              className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 object-contain"
-              src={bowl.image_url}
-            ></img>
-            <button
-              onClick={() => setBowl(bowl)}
-              className="h-12 rounded-xl flex items-center m-2 px-4"
-            >
-              {bowl.name}
-            </button>
-          </div>
-        ))}
+        {bowls.map((bowl) => {
+          const isActive = selectedBowl && selectedBowl.id === bowl.id;
+          return (
+            <div key={bowl.id} className="flex justify-center items-center">
+              <img
+                className={`w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 object-contain ${isActive ? 'ring-4 ring-[#A2D135] border-green-600' : ''}`}
+                src={bowl.image_url}
+                alt={bowl.name}
+              />
+              <button
+                onClick={() => setBowl(bowl)}
+                className={`h-12 rounded-xl flex items-center m-2 px-4 border-2 ${isActive ? 'border-[#A2D135] text-[#A2D135] font-bold' : 'border-transparent'}`}
+              >
+                {bowl.name}
+              </button>
+            </div>
+          );
+        })}
       </div>
     </aside>
   );
