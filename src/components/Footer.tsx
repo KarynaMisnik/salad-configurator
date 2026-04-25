aa3d8c80cea8e4a682b869bf9ecd0b42909c3ae2
KarynaMisnik <karinamisnik94@gmail.com>
2026-04-26 00:12:39 +0300

    footer style updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,38 @@
+import FresseLogo from "../assets/fresse-logo-white.png";
+import OivaLogo from "../assets/oiva-logo.png";
+
 export default function Footer() {
   return (
-    <footer className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
-      <div className="w-full sm:w-1/3 p-4">
-        <p>Fresh Food Factory</p>
-        <p>FRESSE</p>
+    <footer className="bg-[#A2D135] text-white p-8 mt-12 w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 items-start ">
+      <div className="sm:w-1/3 p-4 flex flex-col justify-center items-center">
+        <h2 className="text-lg font-bold mb-2">FRESSE</h2>
+        <img
+          src={FresseLogo}
+          alt="Fresse Logo"
+          className="w-24 sm:w-32 md:w-40 lg:w-48 h-auto object-contain transition-transform hover:scale-105"
+        />
       </div>
-
-      <div className="w-full sm:w-1/3 p-4">
-        <h2 className="text-lg font-bold mb-2">Contact</h2>
-        <p>Phone: 123-456-7890</p>
-        <p>Email: info@example.com</p>
+      <div className="sm:w-1/3 p-4 text-white">
+        <h2 className="text-lg font-bold mb-2 whitespace-nowrap">
+          Ota yhteyttä
+        </h2>
+        <p className="text-lg mb-2 whitespace-nowrap">Phone: 123-456-7890</p>
+        <p className="text-lg mb-2 whitespace-nowrap">
+          Email: info@example.com
+        </p>
       </div>
-
-      <div className="w-full sm:w-1/3 p-4">
-        <h2 className="text-lg font-bold mb-2">Social Media</h2>
+      <div className="sm:w-1/3 p-4 text-whit whitespace-nowrap">
+        <h2 className="text-lg font-bold mb-2">Seuraa meitä</h2>
         <p>Instagram</p>
         <p>Facebook</p>
         <p>Twitter</p>
+      </div>
+      <div className="sm:w-1/3 p-4">
+        <img
+          src={OivaLogo}
+          alt="Oiva Logo"
+          className="w-24 sm:w-32 md:w-40 lg:w-48 h-auto object-contain transition-transform hover:scale-105"
+        />
       </div>
     </footer>
   );
